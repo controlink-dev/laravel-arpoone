f7e8ccc04463a118a2d57bec3d3251fb61684511
João Ribeiro <joao.ribeiro@controlink.pt>
2024-09-27 17:56:24 +0100

    🚀 Fix Notification API Integration for Arpoone Channel

--- src/Channels/Arpoone.php
+++ src/Channels/Arpoone.php
@@ -2,10 +2,54 @@
 
 namespace Controlink\LaravelArpoone\Channels;
 
+use Illuminate\Notifications\Notification;
+
 class Arpoone
 {
-    public function send($notifiable, $notification)
+    public function send(object $notifiable,  Notification $notification)
     {
+        //Check if the $notification has the toArpoone method
+        if (!method_exists($notification, 'toArpoone')) {
+            throw new \Exception('The notification must have a toArpoone method');
+        }
+
+        $message = $notification->toArpoone($notifiable);
+
+        //Check if the $message has the parameters required by the Arpoone API
+        if (!isset($message['to']) || !isset($message['content'])) {
+            throw new \Exception('The message must have a to and content parameters');
+        }
+
         // Send notification to the $notifiable instance
+        // $message['to'] contains the phone number
+        // $message['content'] contains the message
+
+        $url = config('arpoone.url');
+        $apiKey = config('arpoone.api_key');
+        $organizationId = config('arpoone.organization_id');
+        $sender = config('arpoone.sender');
+
+        $messages = [
+            'text' => $message['content'],
+            'to' => $message['to'],
+            'from' => $sender,
+        ];
+
+        $headers = [
+            'Authorization' => 'Bearer ' . $apiKey,
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->post($url . 'sms/send', [
+            'headers' => $headers,
+            'json' => [
+                'organization_id' => $organizationId,
+                'messages' => [$messages],
+            ],
+        ]);
+
+        return $response->getBody();
     }
 }
--- src/config/arpoone.php
+++ src/config/arpoone.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'url' => env('ARPOONE_URL', "https://api.arpoone.com/v1.0/"),
+    'url' => env('ARPOONE_URL', "https://api.arpoone.com/v1.1/"),
     'api_key' => env('ARPOONE_API_KEY', null),
     'organization_id' => env('ARPOONE_ORGANIZATION_ID', null),
     'sender' => env('ARPOONE_SENDER', null),
